adb32c98c1abbbabe6a31ae4a916c294df6579e9
savectrl <savectrlyt@gmail.com>
2024-07-15 16:59:55 -0400

    crazy

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Modal } from '@mui/material';
+import { Modal } from '@material-ui/core';
 
 import SnackBarContent from '../SnackBar/SnackBarContent';
 import PrivateMessageUserList from './PrivateMessageUserList';
